5dec9d59b8c68bf9150495a8d5587f45f946f43e
Drew Short <warrick@sothr.com>
2020-01-22 14:15:12 -0600

    Added debugging

--- assets/common.sh
+++ assets/common.sh
@@ -2,13 +2,18 @@ export TMPDIR=${TMPDIR:-/tmp}
 
 load_config() {
     local config_path=$TMPDIR/rclone_config
+    local rclone_config_path=/opt/rclone/config
+    local rclone_config_file=$rclone_config_path/.rclone.conf
 
     (jq -r '.source.config // empty' < $1) > $config_path
 
     if [ -s $config_path ]; then
-        mkdir -p /opt/rclone/config
-        mv $config_path > /opt/rclone/config/.rclone.conf
-        chmod 500 /opt/rclone/config/.rclone.conf
+        mkdir -p $rclone_config_path
+        mv $config_path $rclone_config_file
+        # TODO: Remove Me - DEBUGGING
+        echo "Config file:"
+        cat $rclone_config_file
+        chmod 500 $rclone_config_file
     else
         echo "No config provided"
         exit 1
